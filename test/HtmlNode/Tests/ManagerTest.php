5fe76b7824b1ecf82784dab094fc52873aaf46b0
ronan-gloo <tessierronan@yahoo.fr>
2013-01-31 18:57:41 +0100

    add Manager tests

---
+++ test/HtmlNode/Tests/ManagerTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace HtmlNode\Tests;
+
+use HtmlNode\Collection\Collection;
+
+class ManagerTest extends \PHPUnit_Framework_TestCase
+{
+	
+	/**
+	 * @dataProvider getClass
+	 */
+	public function testAttributes($class)
+	{
+		$this->assertClassHasStaticAttribute("registry", $class);
+		$this->assertClassHasStaticAttribute("singletons", $class);
+	}
+
+	/**
+	 * @dataProvider getClass
+	 */
+	public function testRegister($class)
+	{
+		$class::register("input", "test");
+		$this->assertTrue($class::registered("input"));
+		
+		$class::register("input2", "test", true);
+		$this->assertTrue($class::registered("input2"));
+	}
+	
+	/**
+	 * @dataProvider getClass
+	 */
+	public function testOnce($class)
+	{
+		$class::once("input", "test");
+		$this->assertTrue($class::registered("input"));
+	}
+	
+	/**
+	 * @dataProvider getClass
+	 */
+	public function testResolveEmpty($class)
+	{
+		$this->assertFalse($class::resolve("foo", []));
+	}
+
+	/**
+	 * @dataProvider getClass
+	 */
+	public function testResolveObject($class)
+	{
+		$class::register("odd", new \stdClass);
+		$this->assertInstanceOf("stdClass", $class::resolve("odd", []));
+	}
+
+	/**
+	 * @dataProvider getClass
+	 */
+	public function testResolveInstance($class)
+	{
+		$class::register("bar", function(){
+			return new \stdClass;
+		});
+		$std = $class::resolve("bar", []);
+		
+		// Test singleton
+		$this->assertNotSame($std, $class::resolve("bar", []));
+	}
+	
+	/**
+	 * @dataProvider getClass
+	 */
+	public function testResolveSingleton($class)
+	{		
+		$class::once("input", function(){
+			return new \stdClass;
+		});
+		
+		$std = $class::resolve("input", []);
+		
+		// Returns the input
+		$this->assertInstanceOf("stdClass", $std);
+		// Test singleton
+		$this->assertSame($std, $class::resolve("input", []));
+	}
+
+	public function getClass()
+	{
+		return [['HtmlNode\Util\Manager']];
+	}
+}
